01c0e712678469129ab467e6d49032a6c16e090b
Sergei Pokhodai <sergei.pokhodai@gmail.com>
2023-03-22 00:21:38 +0400

    commit

--- app/src/main/java/com/android/pokhodai/expensemanagement/ui/TabsFragment.kt
+++ app/src/main/java/com/android/pokhodai/expensemanagement/ui/TabsFragment.kt
@@ -34,11 +34,9 @@ class TabsFragment : BaseFragment<FragmentTabsBinding>(FragmentTabsBinding::infl
     }
 
     private fun initBottomNavigation() = with(binding) {
-        /* Настраивает Multiple Back Stacks */
         bnvTabs.setupWithNavController(navigationController)
         bnvTabs.itemIconTintList = null
 
-        /* Сбрасывает стэк экранов если нажали активную вкладку */
         bnvTabs.setOnItemReselectedListener {
             val option = NavOptions.Builder().setLaunchSingleTop(true).setPopUpTo(
                 navigationController.graph.findStartDestination().id,
@@ -58,23 +56,19 @@ class TabsFragment : BaseFragment<FragmentTabsBinding>(FragmentTabsBinding::infl
         }
 
         navigateToDeepLink.observe(viewLifecycleOwner) { tabDeepLink ->
-            /* Переключается на нужную вкладку */
             binding.bnvTabs.selectedItemId = tabDeepLink.tabId
 
-            /* Ищет граф текущей вкладки */
             val currentGraph = navigationController.graph.firstOrNull {
                 it.id == tabDeepLink.tabId
             }
 
             (currentGraph as? NavGraph)?.let {
-                /* Сбрасывает стэк вкладки до корневого экрана */
                 val option = NavOptions.Builder().setLaunchSingleTop(true).setPopUpTo(
                     it.findStartDestination().id,
                     inclusive = false,
                     saveState = false
                 ).build()
 
-                /* Переходит по диплинку на экран */
                 runCatching {
                     navigationController.navigate(tabDeepLink.uri, option)
                 }
