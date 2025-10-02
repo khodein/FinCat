95ef4a7a22d150fc1e408409d8d0b5e74da760f0
Sergei Pokhodai <s.pokhodai@detmir.ru>
2025-10-02 17:26:42 +0400

    tg token and tg chat id hidden

--- core/network/impl/build.gradle.kts
+++ core/network/impl/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("core-plugin")
     id("org.jetbrains.kotlin.plugin.serialization")
@@ -5,6 +7,30 @@ plugins {
 
 android {
     namespace = "com.sergei.pokhodai.expensemanagement.core.network.impl"
+
+    val localProperties = Properties().apply {
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            load(localPropertiesFile.inputStream())
+        }
+    }
+
+    defaultConfig {
+        buildConfigField(
+            "String",
+            "TG_TOKEN",
+            "\"${localProperties.getProperty("tg.token")}\""
+        )
+        buildConfigField(
+            "String",
+            "TG_CHAT",
+            "\"${localProperties.getProperty("tg.chat")}\""
+        )
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
--- core/network/impl/src/main/java/com/sergei/pokhodai/expensemanagement/core/network/api/QuestionApiServiceImpl.kt
+++ core/network/impl/src/main/java/com/sergei/pokhodai/expensemanagement/core/network/api/QuestionApiServiceImpl.kt
@@ -3,9 +3,9 @@ package com.sergei.pokhodai.expensemanagement.core.network.api
 import com.sergei.pokhodai.expensemanagement.core.network.request.question.QuestionMessageRequest
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import com.sergei.pokhodai.expensemanagement.core.network.impl.BuildConfig
 
 internal class QuestionApiServiceImpl(
     private val httpClient: HttpClient,
@@ -13,11 +13,11 @@ internal class QuestionApiServiceImpl(
 
     override suspend fun sendMessage(message: String) {
         return httpClient.post(
-            urlString = "${DOMAIN}/bot${TOKEN}/${SEND_MESSAGE_METHOD}",
+            urlString = "${DOMAIN}/bot${BuildConfig.TG_TOKEN}/${SEND_MESSAGE_METHOD}",
             block = {
                 setBody(
                     QuestionMessageRequest(
-                        chatId = CHAT_ID,
+                        chatId = BuildConfig.TG_CHAT,
                         message = message,
                     )
                 )
@@ -27,8 +27,6 @@ internal class QuestionApiServiceImpl(
 
     private companion object {
         private const val DOMAIN = "https://api.telegram.org"
-        private const val TOKEN = ""
         private const val SEND_MESSAGE_METHOD = "sendMessage"
-        private const val CHAT_ID = "-1002602154175"
     }
 }
